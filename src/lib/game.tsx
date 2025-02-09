b09c3bc1c0863d38f51f0df1018749d913bd4305
Navinder Singh Mann <102732282+NaviTheCoderboi@users.noreply.github.com>
2025-02-09 16:49:44 +0530

    Update game.tsx

--- src/lib/game.tsx
+++ src/lib/game.tsx
@@ -75,7 +75,7 @@ export const useGame = (): GameState => {
         total: 0,
         current: 0
     });
-    const [strictness, setStrictness] = useState(0.5);
+    const [strictness, setStrictness] = useState(0.9);
     const [category, setCategory] = useState<'all' | string[]>('all');
     const _toMarkPlaces = useMemo(() => getPlace(category), [category]);
     const [toMarkPlaces, setToMarkPlaces] = useState<
